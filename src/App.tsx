14a0911b6dd9626da7e5abf7db1ba9658b0e2dc0
Federico Andrés <lefedeok@gmail.com>
2022-09-08 10:20:24 -0300

    Added a few things

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      Testingg
+      <nav className='navbar'>
+        <ul className='navbar--menu container'>
+          <li className='navbar--menu--item navbar--menu--item-left'><a href='#me'>Logo</a></li>
+          <li className='navbar--menu--item'><a href='#projects'>Projects</a></li>
+          <li className='navbar--menu--item'><a href='#playgrounds'>Playgrounds</a></li>
+          <li className='navbar--menu--item'><a href='#contact'>Contact</a></li>
+        </ul>
+      </nav>
+      <main className='main'>
+
+        <section className='section' id='me'>
+          <h1 className='section--title'>Federico Andres</h1>
+        </section>
+
+        <section className='section' id='playgrounds'>
+          <h1 className='section--title '>Playgrounds</h1>
+        </section>
+
+        <section className='section' id='contact'>
+          <h1 className='section--title '>Contact</h1>
+        </section>
+
+        <section className='section' id='projects'>
+          <h1 className='section--title '>Projects</h1>
+        </section>
+
+
+      </main>
     </div>
   )
 }
